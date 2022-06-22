4109a1a7d652aa8cf53e181f8e6f13b84aacdde2
MihaelaNaidin040696 <naidinmihaela76@gmail.com>
2022-06-22 19:07:01 +0300

    add modal table for residents

--- static/index.js
+++ static/index.js
@@ -3,13 +3,19 @@ const next = document.querySelector('#next');
 let api_url = "https://swapi.py4e.com/api/planets/";
 let next_url = "";
 let previous_url = "";
+let allPlanetsResidents = [];
 
 async function getData(url) {
     const response = await fetch(url);
     let data = await response.json();
     next_url = data['next']
     previous_url = data['previous']
-    insertData(data);
+    insertData(data['results']);
+    for (let planet of data['results']) {
+        const response2 = await fetch(planet['url']);
+        let data2 = await response2.json();
+        allPlanetsResidents.push(data2['url'])
+    }
 }
 
 getData(api_url).then();
@@ -25,37 +31,85 @@ previous.addEventListener('click', () => {
     }
 });
 
-function insertData(data) {
-    let tab = '';
-    for (const item of data.results) {
-        if (isNaN(item.surface_water)) {
-            item.surface_water = 'unknown'
+function insertData(planets) {
+    let planetsTable = '';
+    for (let planet of planets) {
+        if (isNaN(planet.surface_water)) {
+            planet.surface_water = 'unknown'
         } else {
-            item.surface_water += '%'
+            planet.surface_water += '%'
         }
-        if (isNaN(item.population)) {
-            item.population = 'unknown'
+        if (isNaN(planet.population)) {
+            planet.population = 'unknown'
         } else {
-            item.population = parseFloat(item.population).toLocaleString('en') + ' people'
+            planet.population = parseFloat(planet.population).toLocaleString('en') + ' people'
         }
-        if (isNaN(item.diameter)) {
-            item.diameter = 'unknown'
+        if (isNaN(planet.diameter)) {
+            planet.diameter = 'unknown'
         } else {
-            item.diameter = parseFloat(item.diameter).toLocaleString('en') + ' km'
+            planet.diameter = parseFloat(planet.diameter).toLocaleString('en') + ' km'
         }
-        if (item.residents.length <= 0) {
-            item.residents = 'No known residents'
+        if (planet.residents.length <= 0) {
+            planet.residents = 'No known residents'
         } else {
-            item.residents = item.residents.length + " resident(s)"
+            planet.residents = `<button type="button" class="btn btn-secondary btn-sm" onclick="insertResidents('${planet.url}', '${planet.name}')" data-toggle="modal" data-target="#exampleModal">` + planet.residents.length + " resident(s)" + `</button>`
         }
-        tab += "<tr>";
-        tab += "<td>" + item.name + "</td>";
-        tab += "<td>" + item.diameter +"</td>";
-        tab += "<td>" + item.climate + "</td>";
-        tab += "<td>" + item.terrain + "</td>";
-        tab += "<td>" + item.surface_water + "</td>";
-        tab += "<td>" + item.population +"</td>";
-        tab += "<td>" + item.residents + "</td>";
+        planetsTable += "<tr>";
+        planetsTable += "<td>" + planet.name + "</td>";
+        planetsTable += "<td>" + planet.diameter +"</td>";
+        planetsTable += "<td>" + planet.climate + "</td>";
+        planetsTable += "<td>" + planet.terrain + "</td>";
+        planetsTable += "<td>" + planet.surface_water + "</td>";
+        planetsTable += "<td>" + planet.population +"</td>";
+        planetsTable += "<td>" + planet.residents + "</td>";
+        planetsTable += "</tr>";
+    }
+    document.querySelector('#data').innerHTML = planetsTable;
 }
-    document.querySelector('#data').innerHTML = tab;
+
+
+async function insertResidents(planetUrl, planetName) {
+    let peopleDetails = [];
+    for (let resident of allPlanetsResidents) {
+        if (resident === planetUrl) {
+            const response3 = await fetch(resident);
+            const residentDetails = await response3.json();
+            for (let residentDetail of residentDetails['residents']) {
+                const response4 = await fetch(residentDetail);
+                const details = await response4.json();
+                peopleDetails.push(details);
+            }
+        }
+    }
+    let residentsTable = '';
+    for (let resident of peopleDetails) {
+        if (resident.hair_color === 'n/a' || resident.hair_color === 'none'){
+            resident.hair_color = 'unknown'
+        }
+        if (resident.gender === 'n/a'){
+            resident.gender = 'unknown'
+        }
+        if (isNaN(resident.mass)) {
+            resident.mass = 'unknown'
+        } else {
+            resident.mass = parseFloat(resident.mass).toLocaleString('en') + ' kg'
+        }
+        if (isNaN(resident.height)) {
+            resident.height = 'unknown'
+        } else {
+            resident.height = parseFloat(resident.height).toLocaleString('en') + ' m'
+        }
+        residentsTable += "<tr>";
+        residentsTable += "<td>" + resident.name + "</td>";
+        residentsTable += "<td>" + resident.height + "</td>";
+        residentsTable += "<td>" + resident.mass + "</td>";
+        residentsTable += "<td>" + resident.hair_color + "</td>";
+        residentsTable += "<td>" + resident.skin_color + "</td>";
+        residentsTable += "<td>" + resident.eye_color + "</td>";
+        residentsTable += "<td>" + resident.birth_year + "</td>";
+        residentsTable += "<td>" + resident.gender + "</td>";
+        residentsTable += "</tr>";
+    }
+    document.querySelector('#resident').innerHTML = residentsTable;
+    document.querySelector('.modal-title').innerHTML = "Residents of " + planetName;
 }
