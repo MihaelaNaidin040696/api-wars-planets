42072d05c0e31622f12023a8c806843b48cb62dc
Mihaela Naidin <98595633+MihaelaNaidin040696@users.noreply.github.com>
2023-02-13 18:35:51 +0200

    Development (#1)
    * create gitignore file
    
    * Delete .idea directory
    
    * create connection file
    
    * add login and register functionalities
    
    * add requirements file
    
    * add hashed password functions
    
    * add query for insert user details in database, read user details and get password
    
    * add base template
    
    * add login template
    
    * add registration template
    
    * add index template for main page
    
    * add js file for index page, created table
    
    * add css style for table
    
    * add modal for residents
    
    * restore table css
    
    * add modal table for residents
    
    * add navbar for login or logout and register links
    
    * changed redirect for logout route
    
    * add vote planet functionality and store votes in database
    
    * create sql scripts for database
    
    * add voting statistics functionality
    
    * add query to get votes from database
    
    * add modal for voting statistics
    
    * css styling
    
    * small cleanup
    
    * create files for heroku deployment

--- server.py
+++ server.py
@@ -1,1 +1,80 @@
-# put your code here
+from flask import Flask, render_template, request, url_for, redirect, session, jsonify
+import data_manager
+import hash_pass
+import re
+import ast
+
+app = Flask(__name__)
+app.secret_key = "_5#y2LF4Q8z\xec]/"
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        user_credentials = data_manager.get_user(username)
+        if not username or not password:
+            msg = 'Please, fill in both fields.'
+        elif user_credentials:
+            msg = 'Username already exists, please choose another one!'
+        elif re.match(r"^[A-Za-z\d]$", username):
+            msg = 'Username must contain only characters and numbers!'
+        else:
+            password = hash_pass.hash_password(password)
+            data_manager.insert_user_credentials(username, password)
+            return redirect(url_for('login'))
+    elif request.method == 'POST':
+        msg = 'Please fill out the form!'
+    return render_template(
+        'register.html', msg=msg
+    )
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    msg = ''
+    if request.method == 'POST' and (not request.form['username'] or not request.form['password']):
+        msg = 'Please, fill in both fields.'
+        return render_template('login.html', msg=msg)
+    elif request.method == 'POST' \
+            and 'username' in request.form \
+            and 'password' in request.form \
+            and hash_pass.verify_password(request.form['password'],
+                                          data_manager.get_user_pass(request.form['username'])):
+        user_credentials = data_manager.get_user(request.form['username'])
+        if user_credentials:
+            session['loggedin'] = True
+            session['id'] = user_credentials['id']
+            session['username'] = user_credentials['username']
+            return redirect(url_for('index'))
+        msg = 'Wrong username or password.'
+    return render_template('login.html', msg=msg)
+
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
+
+
+@app.route('/vote', methods=['GET', 'POST'])
+def vote_planet():
+    data = ast.literal_eval(request.data.decode("utf-8"))
+    data_manager.insert_vote(int(data.get('id')), data.get('name'), int(session['id']))
+    return jsonify(added=True)
+
+
+@app.route('/voting-statistics')
+def voting_statistics():
+    return jsonify(data_manager.get_voting_statistics(session['id']))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ static/index.js
@@ -0,0 +1,163 @@
+const previous = document.querySelector('#prev');
+const next = document.querySelector('#next');
+let api_url = "https://swapi.py4e.com/api/planets/";
+let next_url = "";
+let previous_url = "";
+let allPlanetsResidents = [];
+
+
+
+function logged_in() {
+    const username = document.querySelector('#username');
+    window.sessionStorage.setItem('logged_in', username.value);
+}
+
+function logged_out() {
+    window.sessionStorage.removeItem('logged_in');
+}
+
+async function getData(url) {
+    const response = await fetch(url);
+    let data = await response.json();
+    next_url = data['next']
+    previous_url = data['previous']
+    insertData(data['results']);
+    for (let planet of data['results']) {
+        const response2 = await fetch(planet['url']);
+        let data2 = await response2.json();
+        allPlanetsResidents.push(data2['url'])
+    }
+}
+
+getData(api_url).then();
+
+next.addEventListener('click', () => {
+    if (next_url){
+        getData(next_url).then();
+    }
+});
+previous.addEventListener('click', () => {
+    if (previous_url) {
+        getData(previous_url).then();
+    }
+});
+
+function insertData(planets) {
+    let planetsTable = '';
+    for (let planet of planets) {
+        if (isNaN(planet.surface_water)) {
+            planet.surface_water = 'unknown'
+        } else {
+            planet.surface_water += '%'
+        }
+        if (isNaN(planet.population)) {
+            planet.population = 'unknown'
+        } else {
+            planet.population = parseFloat(planet.population).toLocaleString('en') + ' people'
+        }
+        if (isNaN(planet.diameter)) {
+            planet.diameter = 'unknown'
+        } else {
+            planet.diameter = parseFloat(planet.diameter).toLocaleString('en') + ' km'
+        }
+        if (planet.residents.length <= 0) {
+            planet.residents = 'No known residents'
+        } else {
+            planet.residents = `<button type="button" class="btn btn-secondary btn-sm" onclick="insertResidents('${planet.url}', '${planet.name}')" data-toggle="modal" data-target="#exampleModal">` + planet.residents.length + " resident(s)" + `</button>`
+        }
+        planetsTable += "<tr>";
+        planetsTable += "<td>" + planet.name + "</td>";
+        planetsTable += "<td>" + planet.diameter +"</td>";
+        planetsTable += "<td>" + planet.climate + "</td>";
+        planetsTable += "<td>" + planet.terrain + "</td>";
+        planetsTable += "<td>" + planet.surface_water + "</td>";
+        planetsTable += "<td>" + planet.population +"</td>";
+        planetsTable += "<td>" + planet.residents + "</td>";
+        if (window.sessionStorage.getItem('logged_in')){
+            planetsTable += `<td><button type="button" onclick="getPlanetNameAndID('${planet.url}','${planet.name}')" class="btn btn-secondary btn-sm" >` + 'Vote' + `</button></td>`;
+        }
+        planetsTable += "</tr>";
+    }
+    document.querySelector('#data').innerHTML = planetsTable;
+}
+
+
+async function insertResidents(planetUrl, planetName) {
+    let peopleDetails = [];
+    for (let resident of allPlanetsResidents) {
+        if (resident === planetUrl) {
+            const response3 = await fetch(resident);
+            const residentDetails = await response3.json();
+            for (let residentDetail of residentDetails['residents']) {
+                const response4 = await fetch(residentDetail);
+                const details = await response4.json();
+                peopleDetails.push(details);
+            }
+        }
+    }
+    let residentsTable = '';
+    for (let resident of peopleDetails) {
+        if (resident.hair_color === 'n/a' || resident.hair_color === 'none'){
+            resident.hair_color = 'unknown'
+        }
+        if (resident.gender === 'n/a'){
+            resident.gender = 'unknown'
+        }
+        if (isNaN(resident.mass)) {
+            resident.mass = 'unknown'
+        } else {
+            resident.mass = parseFloat(resident.mass).toLocaleString('en') + ' kg'
+        }
+        if (isNaN(resident.height)) {
+            resident.height = 'unknown'
+        } else {
+            resident.height = parseFloat(resident.height).toLocaleString('en') + ' m'
+        }
+        residentsTable += "<tr>";
+        residentsTable += "<td>" + resident.name + "</td>";
+        residentsTable += "<td>" + resident.height + "</td>";
+        residentsTable += "<td>" + resident.mass + "</td>";
+        residentsTable += "<td>" + resident.hair_color + "</td>";
+        residentsTable += "<td>" + resident.skin_color + "</td>";
+        residentsTable += "<td>" + resident.eye_color + "</td>";
+        residentsTable += "<td>" + resident.birth_year + "</td>";
+        residentsTable += "<td>" + resident.gender + "</td>";
+        residentsTable += "</tr>";
+    }
+    document.querySelector('#resident').innerHTML = residentsTable;
+    document.querySelector('.modal-title').innerHTML = "Residents of " + planetName;
+}
+
+function getPlanetNameAndID(url,name){
+    let id = url.split('planets/')[1].substring(0,1)
+    let data = {id: id,
+        name: name}
+    insertPlanetVote(data).then();
+}
+
+async function insertPlanetVote(data) {
+    await fetch('/vote', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    })
+}
+
+async function getVoteStatistics() {
+    const response = await fetch('/voting-statistics');
+    const data = await response.json();
+    insertVotes(data);
+}
+
+function insertVotes(data) {
+    let votesTable ='';
+    for (let vote of data) {
+        votesTable += "<tr>";
+        votesTable += "<td>" + vote.planet_name + "</td>";
+        votesTable += "<td>" + vote.count + "</td>";
+        votesTable += "</tr>";
+    }
+     document.querySelector('#votes').innerHTML = votesTable;
+}
